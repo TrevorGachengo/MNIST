4bcb76fc571d9b7f6dcfca206277d3ffce8f74d3
Trevor Gachengo <tgachengo@gmail.com>
2024-11-04 00:18:40 +0000

    Update main.py

--- network/main.py
+++ network/main.py
@@ -3,7 +3,7 @@ from network import Network
 
 train, test = mnist(True)
 
-net = Network(300, mini_batch_size=2048, learning_rate=1e-3)
+net = Network(300, batch_size=2048, learning_rate=1e-3)
 net.epoch(100, train, test, fixed_learning_rate=False, display=True)   
 
 net.save_parameters()
